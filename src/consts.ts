d856f8613cb97ece13fac61d09dfe89b2b4bd9ab
Dongpil Jo <dpjo@miridih.com>
2026-04-19 21:48:08 +0900

    initial commit: i18n 구조 설계 가이드 (AEO 최적화 블로그)

---
+++ src/consts.ts
@@ -0,0 +1,12 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '조동필 · Dev Blog';
+export const SITE_DESCRIPTION = '프론트엔드 엔지니어 조동필의 개발 블로그. i18n, 렌더링 최적화, 디자인 시스템을 다룹니다.';
+export const AUTHOR = {
+	name: '조동필',
+	jobTitle: 'Frontend Engineer',
+	affiliation: 'Miridih',
+	url: 'https://github.com/eastfilmm',
+	email: 'dpjo@miridih.com',
+};
